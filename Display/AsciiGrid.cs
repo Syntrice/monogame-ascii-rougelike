0015591c575cb95a824f9692721931474f790ce0
syntrice <76839403+Syntrice@users.noreply.github.com>
2024-10-26 15:30:56 +0100

    Begin implementation of an Ascii Graphics renderer
    The AsciiGrid class will be used to keep track of a grid display of characters and colors. These will
    be able to be drawn to the screen using the MonoGame functionality. A font will have to be used to do this.

---
+++ Display/AsciiGrid.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended;
+
+namespace AsciiRougelike.Display
+{
+    internal class AsciiGrid
+{
+        int Width { get; }
+        int Height { get; }
+
+        char[,] BackgroundChars { get; set; }
+        char[,] ForegroundChars { get; set; }
+
+        Color[,] BackgroundColors { get; set; }
+
+        Color[,] ForegroundColors { get; set; }
+
+
+        public AsciiGrid(int width, int height)
+        {
+            Width = width;
+            Height = height;
+
+            // Initialize arrays
+            BackgroundChars = new char[Width, Height];
+            ForegroundChars = new char[Width, Height];
+            BackgroundColors = new Color[Width, Height];
+            ForegroundColors = new Color[Width, Height];
+
+        }
+
+        public void Render()
+        {
+
+        }
+    }
+}
